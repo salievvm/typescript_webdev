2ffe7b568e737e98d96587150e7f69c3df9cc9ab
salievvm <salievvm@gmail.com>
2022-02-21 20:53:04 +0300

    1. Basic types

--- src/1. Basic types/index.js
+++ src/1. Basic types/index.js
@@ -1,1 +1,19 @@
-console.log();
+// 1. Boolean
+var isCompleted = false;
+isCompleted = true;
+// 2. Number Type
+var decimal = 6;
+var integer = 7.10;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+// 3. String
+var str = "Hello ".concat(4);
+var str1 = 'Bye';
+// 4. Null undefined
+var un = undefined;
+var nu = null; // object in JS
+// 5. Void
+var greetUser = function (user) {
+    alert("Hello, ".concat(user));
+};
--- src/1. Basic types/index.ts
+++ src/1. Basic types/index.ts
@@ -1,1 +1,27 @@
-console.log();
+// 1. Boolean
+let isCompleted: boolean = false;
+isCompleted = true;
+
+// 2. Number Type
+
+const decimal: number = 6;
+const integer: number = 7.10;
+const hex: number = 0xf00d;
+const binary: number = 0b1010;
+const octal: number = 0o744;
+
+// 3. String
+
+const str: string = `Hello ${4}`;
+const str1: string = 'Bye';
+
+// 4. Null undefined
+
+const un: undefined = undefined;
+const nu: null = null; // object in JS
+
+// 5. Void
+
+const greetUser = (user: string): void => {
+    alert(`Hello, ${user}`);
+}
